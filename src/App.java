1fe98de5dfdbaba98ab4b1ce7a804e2b3c7bc4a4
Adrian Gutierrez <capitanadriang@gmail.com>
2025-09-25 12:43:00 +0200

    added pyramid

--- src/App.java
+++ src/App.java
@@ -1,5 +1,9 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.printf("%5s%n","*" );
+        System.out.printf("%6s%n","***" );
+        System.out.printf("%7s%n","*****" );
+        System.out.printf("%8s%n","*******" );
+        System.out.print("*********" );
     }
 }
